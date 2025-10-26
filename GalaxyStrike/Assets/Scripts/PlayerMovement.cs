c5807454f0833dd8f4e2a29b466651bf291e1264
Max Valle <55932182+Valle94@users.noreply.github.com>
2025-10-26 19:04:26 -0400

    4.13 Rotating Pitch
    Using the same method as for roll, added a visual pitch change when the player is moving up and down.

--- GalaxyStrike/Assets/Scripts/PlayerMovement.cs
+++ GalaxyStrike/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float yClampRange = 10f;
 
     [SerializeField] private float controlRollFactor = 30f;
+    [SerializeField] private float controlPitchFactor = 15f;
     [SerializeField] private float rotationSpeed = 10f;
 
     Vector2 movement;
@@ -38,7 +39,9 @@ public class PlayerMovement : MonoBehaviour
 
     private void ProcessRotation()
     {
-        Quaternion targetRotation = Quaternion.Euler(0f, 0f, controlRollFactor * -movement.x);
+        float pitch = controlPitchFactor * -movement.y;
+        float roll = controlRollFactor * -movement.x;
+        Quaternion targetRotation = Quaternion.Euler(pitch, 0f, roll);
         transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
