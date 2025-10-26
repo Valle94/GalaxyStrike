0f12ea366401fbc53cbb7be28f8aa5d543505c4f
Max Valle <55932182+Valle94@users.noreply.github.com>
2025-10-26 18:54:10 -0400

    4.11 Roll Ship
    Using a quaternion, added a "roll" to the ship whenever the player is moving left or right.

--- GalaxyStrike/Assets/Scripts/PlayerMovement.cs
+++ GalaxyStrike/Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,40 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float controlSpeed = 50f;
+    [SerializeField] private float xClampRange = 10f;
+    [SerializeField] private float yClampRange = 10f;
+
+    [SerializeField] private float controlRollFactor = 30f;
 
     Vector2 movement;
 
     void Update()
     {
         ProcessTranslation();
+        ProcessRotation();
     }
 
     public void OnMove(InputValue value)
     {
         movement = value.Get<Vector2>();
     }
-    
+
     private void ProcessTranslation()
     {
         float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
+
         float yOffset = movement.y * controlSpeed * Time.deltaTime;
-        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
+    }
+
+    private void ProcessRotation()
+    {
+        Quaternion targetRotation = Quaternion.Euler(0f, 0f, controlRollFactor * -movement.x);
+        transform.localRotation = targetRotation;
     }
 }
