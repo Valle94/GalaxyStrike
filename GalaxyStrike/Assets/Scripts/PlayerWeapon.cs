d5a84dcabadf2621d6180fabc173e5b08e8b7927
Max Valle <55932182+Valle94@users.noreply.github.com>
2025-10-29 15:58:57 -0400

    4.19 Vector Arithmetic
    Implemented some vector math to actually point our lasers at the ball in the distance.

--- GalaxyStrike/Assets/Scripts/PlayerWeapon.cs
+++ GalaxyStrike/Assets/Scripts/PlayerWeapon.cs
@@ -19,6 +19,7 @@ public class PlayerWeapon : MonoBehaviour
         ProcessFiring();
         MoveCrosshair();
         MoveTargetPoint();
+        AimLasers();
     }
 
     private void OnFire(InputValue value)
@@ -45,5 +46,15 @@ public class PlayerWeapon : MonoBehaviour
         Vector3 targetPointPosition = new Vector3(Mouse.current.position.x.ReadValue(),
                                                 Mouse.current.position.y.ReadValue(), targetDistance);
         targetPoint.position = Camera.main.ScreenToWorldPoint(targetPointPosition);
+    }
+
+    private void AimLasers()
+    {
+        foreach (ParticleSystem laser in lasers)
+        {
+            Vector3 fireDirection = targetPoint.position - transform.position;
+            Quaternion rotationToTarget = Quaternion.LookRotation(fireDirection);
+            laser.transform.rotation = rotationToTarget;
+        }
     }
 }
