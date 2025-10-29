07b7c43a68e2df80d9e77850eba3a3d8386c51b1
Max Valle <55932182+Valle94@users.noreply.github.com>
2025-10-29 15:52:20 -0400

    4.18 ScreenToWorldPoint Target
    Implemented a "target" that we can use to point out lasers in a particular direction using screen to world point.

--- GalaxyStrike/Assets/Scripts/PlayerWeapon.cs
+++ GalaxyStrike/Assets/Scripts/PlayerWeapon.cs
@@ -1,22 +1,31 @@
 using UnityEngine;
-using UnityEngine.Analytics;
 using UnityEngine.InputSystem;
 
 public class PlayerWeapon : MonoBehaviour
 {
-    [SerializeField] ParticleSystem[] lasers;
+    [SerializeField] private ParticleSystem[] lasers;
+    [SerializeField] RectTransform crosshair;
+    [SerializeField] Transform targetPoint;
+    [SerializeField] float targetDistance;
     private bool isFiring = false;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
 
     void Update()
     {
         ProcessFiring();
+        MoveCrosshair();
+        MoveTargetPoint();
     }
 
     private void OnFire(InputValue value)
     {
         isFiring = value.isPressed;
     }
-    
+
     private void ProcessFiring()
     {
         foreach (ParticleSystem laser in lasers)
@@ -24,5 +33,17 @@ public class PlayerWeapon : MonoBehaviour
             ParticleSystem.EmissionModule emissionModule = laser.GetComponent<ParticleSystem>().emission;
             emissionModule.enabled = isFiring;
         }
+    }
+
+    private void MoveCrosshair()
+    {
+        crosshair.position = Mouse.current.position.ReadValue();
+    }
+
+    private void MoveTargetPoint()
+    {
+        Vector3 targetPointPosition = new Vector3(Mouse.current.position.x.ReadValue(),
+                                                Mouse.current.position.y.ReadValue(), targetDistance);
+        targetPoint.position = Camera.main.ScreenToWorldPoint(targetPointPosition);
     }
 }
