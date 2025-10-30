f495194976801e2bcb0de492f0456431cb72ff37
Max Valle <55932182+Valle94@users.noreply.github.com>
2025-10-29 20:25:23 -0400

    4.22 Instantiate at Runtime
    Created and instantiated a particle system that plays when we destroy an enemy.

--- GalaxyStrike/Assets/Scripts/Enemy.cs
+++ GalaxyStrike/Assets/Scripts/Enemy.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] ParticleSystem destroyedVFX;
+
     void OnParticleCollision(GameObject other)
     {
+        Instantiate(destroyedVFX, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
