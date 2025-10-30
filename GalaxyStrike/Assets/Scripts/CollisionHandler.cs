08b707ab5a9ea7c93742c9bdeb84f2d26c4a8c18
Max Valle <55932182+Valle94@users.noreply.github.com>
2025-10-29 20:59:47 -0400

    4.23 Nested Prefabs & Prefab Variants
    Created prefab variants of my ship explosion VFX and applied different versions to the enemy ships and the player ship.

--- GalaxyStrike/Assets/Scripts/CollisionHandler.cs
+++ GalaxyStrike/Assets/Scripts/CollisionHandler.cs
@@ -2,20 +2,11 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    [SerializeField] private ParticleSystem playerDestroyVFX;
 
     void OnTriggerEnter(Collider other)
     {
+        Instantiate(playerDestroyVFX, gameObject.transform.position, Quaternion.identity);
         Debug.Log($"Hit {other.name}");
     }
 }
