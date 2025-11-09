cf012409eefbcbbc885f301f6908b1aa547ffb1e
Max Valle <55932182+Valle94@users.noreply.github.com>
2025-11-09 10:57:24 -0500

    4.27 Public Methods C#
    Added a publicly accessible score method.

--- GalaxyStrike/Assets/Scripts/Enemy.cs
+++ GalaxyStrike/Assets/Scripts/Enemy.cs
@@ -4,7 +4,15 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] ParticleSystem destroyedVFX;
     [SerializeField] int hitPoints = 3;
+    [SerializeField] int scoreValue = 10;
 
+    Scoreboard scoreboard;
+
+    void Start()
+    {
+        scoreboard = FindFirstObjectByType<Scoreboard>();
+    }
+    
     void OnParticleCollision(GameObject other)
     {
         ProcessHit();
@@ -16,6 +24,7 @@ public class Enemy : MonoBehaviour
 
         if (hitPoints <= 0)
         {
+            scoreboard.IncreaseScore(scoreValue);
             Instantiate(destroyedVFX, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
